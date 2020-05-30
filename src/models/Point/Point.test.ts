4a4bf7d0eff4e67171d968121c84440014dbdd31
vreezy117 <vreezy117@googlemail.com>
2020-05-30 13:46:23 +0200

    model: Glyph, Point, Portal

--- src/models/Point/Point.test.ts
+++ src/models/Point/Point.test.ts
@@ -13,9 +13,13 @@ class TestGlyph extends Glyph {
   }
 }
 
+// create testGlyph
 const testGlyph: Glyph = new TestGlyph();
-const point1: Point = new Point();
-const point2: Point = new Point();
+
+// create classic
+const point1: Point = new Point(50.929868,6.37841);
+const point2: Point = new Point(50.929868,-190);
+const point3: Point = new Point(90,6.37841);
 
 // uuidv4 test
 // https://gist.github.com/johnelliott/cf77003f72f889abbc3f32785fa3df8d
@@ -24,14 +28,51 @@ const v4 = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[
 test('public toString', () => {
    expect(point1.toString()).toMatch(v4);
    expect(point2.toString()).toMatch(v4);
+   expect(point3.toString()).toMatch(v4);
    expect(testGlyph.toString()).toMatch(v4);
 });
 
 test('static pointCount', () => {
-  expect(Point.pointCount).toBe(2);
+  expect(Point.pointCount).toBe(3);
 });
 
 test('static count', () => {
-  expect(Point.count).toBe(3);
-  expect(Glyph.count).toBe(3);
+  expect(Point.count).toBe(4);
+  expect(Glyph.count).toBe(4);
+});
+
+test('lat & lng', () => {
+  expect(point1.lat).toBe(50.929868);
+  expect(point1.lng).toBe(6.37841);
+  expect(point2.lat).toBe(50.929868);
+  expect(point2.lng).toBe(-190);
+  expect(point3.lat).toBe(90);
+  expect(point3.lng).toBe(6.37841);
+});
+
+test('lat & lng as array', () => {
+  expect(point1.getArr()).toBeInstanceOf(Array);
+  expect(point2.getArr()).toBeInstanceOf(Array);
+  expect(point3.getArr()).toBeInstanceOf(Array);
+  expect(point1.getArr()).toStrictEqual([50.929868, 6.37841]);
+  expect(point2.getArr()).toStrictEqual([50.929868,-190]);
+  expect(point3.getArr()).toStrictEqual([90,6.37841]);
+});
+
+test('lat & lng as object', () => {
+  expect(point1.getObj()).toBeInstanceOf(Object);
+  expect(point2.getObj()).toBeInstanceOf(Object);
+  expect(point3.getObj()).toBeInstanceOf(Object);
+  expect(point1.getObj()).toStrictEqual({lat: 50.929868, lng: 6.37841});
+  expect(point2.getObj()).toStrictEqual({lat: 50.929868, lng: -190});
+  expect(point3.getObj()).toStrictEqual({lat: 90, lng: 6.37841});
+});
+
+test('public & checkPoint', () => {
+  expect(point1.checkPoint()).toBe(true);
+  expect(point2.checkPoint()).toBe(false);
+  expect(point3.checkPoint()).toBe(false);
+  expect(Point.checkPoint(50.929868,6.37841)).toBe(true);
+  expect(Point.checkPoint(50.929868,-190)).toBe(false);
+  expect(Point.checkPoint(90,6.37841)).toBe(false);
 });
--- src/models/Point/Point.ts
+++ src/models/Point/Point.ts
@@ -3,10 +3,16 @@ import Glyph from '../Glyph/Glyph';
 export default class Point extends Glyph {
   static pointCount = 0;
 
-  protected lat: number;
+  readonly lat: number;
 
-  protected lng: number;
+  readonly lng: number;
 
+  /**
+   * Creates an instance of Point.
+   * @param {number} lat
+   * @param {number} lng
+   * @memberof Point
+   */
   constructor(lat: number, lng: number) {
     super();
     Point.pointCount += 1;
@@ -14,24 +20,72 @@ export default class Point extends Glyph {
     this.lng = lng;
   }
 
+  /**
+   * Check if Lat is on Earth
+   *
+   * @param {number} lat
+   * @static
+   * @memberof Point
+   */
   public static checkLat = (lat: number): boolean => {
     if (lat > -85.05112878 && lat < 85.05112878) {
       return true;
     }
     return false;
-  }
+  };
 
+  /**
+   * Check if Lng is on Earth
+   *
+   * @param {number} lng
+   * @static
+   * @memberof Point
+   */
   public static checkLng = (lng: number): boolean => {
     if (lng > -180 && lng < 180) {
       return true;
     }
     return false;
-  }
+  };
 
-  public checkPoint = () => {
-    if (Point.checkLat(this.lat) && Point.checkLng(this.lng)) {
+  /**
+   * Check if Point is on Earth
+   *
+   * @param {number} lat
+   * @param {number} lng
+   * @static
+   * @memberof Point
+   */
+  static checkPoint = (lat: number, lng: number) => {
+    if (Point.checkLat(lat) && Point.checkLng(lng)) {
       return true;
     }
     return false;
-  }
+  };
+
+  /**
+   * Check if Point is on Earth
+   *
+   * @param {number} lat
+   * @param {number} lng
+   * @memberof Point
+   */
+  public checkPoint = (lat = this.lat, lng = this.lng) => Point.checkPoint(lat, lng);
+
+  /**
+   * Return an Array with [lat, lng]
+   *
+   * @memberof Point
+   */
+  public getArr = (): number[] => [this.lat, this.lng];
+
+  /**
+   * Returns an Oject with {lat, lng}
+   *
+   * @memberof Point
+   */
+  public getObj = () => ({
+    lat: this.lat,
+    lng: this.lng,
+  });
 }
